fc17a777e898a119292a841bc1511e6e66449560
Simplyareed <adam.reed1991@gmail.com>
2024-06-20 21:43:29 -0700

    changed code inside of database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,24 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jatedb = await openDB('jate', 1);
+  const tx = jatedb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  await store.put({ content });
+  await tx.done;
+}
+  //console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jatedb = await openDB('jate', 1);
+  const tx = jatedb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const allContent = await store.getAll();
+  await tx.done;
+  return allContent && allContent.length > 0 ? allContent[allContent.length -1].content : null;
+}
+  //console.error('getDb not implemented');
 
 initdb();
